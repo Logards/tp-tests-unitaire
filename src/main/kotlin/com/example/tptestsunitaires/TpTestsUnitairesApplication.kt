743a930eb5f000ca7c5306158861b1d3ba254fda
Bastien-DA <deamezagabastien@outlook.fr>
2026-05-26 09:54:26 +0200

    feat: init

---
+++ src/main/kotlin/com/example/tptestsunitaires/TpTestsUnitairesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tptestsunitaires
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TpTestsUnitairesApplication
+
+fun main(args: Array<String>) {
+    runApplication<TpTestsUnitairesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tptestsunitaires/TpTestsUnitairesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tptestsunitaires
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TpTestsUnitairesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
